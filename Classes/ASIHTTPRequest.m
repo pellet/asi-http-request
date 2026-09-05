06a265925af86b8769a53dfe0428da2e10447f83
Ben Pettit <benpettit@digimulti.com>
2026-09-05 11:45:03 +1000

    Send SNI when certificate validation is relaxed
    Passing kCFNull as kCFStreamSSLPeerName switched off server name
    indication along with the hostname check, so any host that picks its
    certificate by name aborted the handshake. Naming the host keeps the
    relaxed validation and lets those servers answer.

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -1218,11 +1218,12 @@ static NSOperationQueue *sharedQueue = nil;
         if (![self validatesSecureCertificate]) {
             // see: http://iphonedevelopment.blogspot.com/2010/05/nsstream-tcp-and-ssl.html
             
+            NSString *peerName = [[self url] host];
             NSDictionary *sslProperties = [[NSDictionary alloc] initWithObjectsAndKeys:
                                       [NSNumber numberWithBool:YES], kCFStreamSSLAllowsExpiredCertificates,
                                       [NSNumber numberWithBool:YES], kCFStreamSSLAllowsAnyRoot,
                                       [NSNumber numberWithBool:NO],  kCFStreamSSLValidatesCertificateChain,
-                                      kCFNull,kCFStreamSSLPeerName,
+                                      (peerName ? (id)peerName : (id)kCFNull),kCFStreamSSLPeerName,
                                       nil];
             
             CFReadStreamSetProperty((CFReadStreamRef)[self readStream], 
